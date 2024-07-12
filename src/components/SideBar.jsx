86584f4154fd3ead5251d48de3903482df4eaa7d
Robin <robin.sliet@gmail.com>
2024-07-12 22:48:33 +0530

    Implemented Design of RightBar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,16 +5,16 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  Switch
+  Switch,
 } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
-import ArticleIcon from '@mui/icons-material/Article';
-import GroupIcon from '@mui/icons-material/Group';
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import Person4Icon from '@mui/icons-material/Person4';
-import Settings from '@mui/icons-material/Settings';
-import AccountBox from '@mui/icons-material/AccountBox';
-import ModeNightIcon from '@mui/icons-material/ModeNight';
+import ArticleIcon from "@mui/icons-material/Article";
+import GroupIcon from "@mui/icons-material/Group";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import Person4Icon from "@mui/icons-material/Person4";
+import Settings from "@mui/icons-material/Settings";
+import AccountBox from "@mui/icons-material/AccountBox";
+import ModeNightIcon from "@mui/icons-material/ModeNight";
 
 const listItems = [
   { icon: <HomeIcon />, text: "Home" },
@@ -27,38 +27,35 @@ const listItems = [
 ];
 function SideBar() {
   return (
-    <Box
-      // bgcolor={"skyblue"}
-      flex={1}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      <List>
-        {listItems.map((item) => (
-          <>
-            <ListItem disablePadding>
-              <ListItemButton component="a" href="#">
-                <ListItemIcon>
-                  {item.icon}
-                </ListItemIcon>
-                <ListItemText primary={item.text} />
-              </ListItemButton>
-            </ListItem>
-          </>
-        ))}
-            <ListItem disablePadding>
-              <ListItemButton sx={{
-              "&:hover": {
-                backgroundColor: "transparent",
-              },
-            }}>
-                <ListItemIcon>
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed">
+        <List>
+          {listItems.map((item) => (
+            <>
+              <ListItem disablePadding>
+                <ListItemButton component="a" href="#">
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            </>
+          ))}
+          <ListItem disablePadding>
+            <ListItemButton
+              sx={{
+                "&:hover": {
+                  backgroundColor: "transparent",
+                },
+              }}
+            >
+              <ListItemIcon>
                 <ModeNightIcon />
-                </ListItemIcon>
-                <Switch defaultChecked />
-              </ListItemButton>
-            </ListItem>
-      </List>
+              </ListItemIcon>
+              <Switch defaultChecked />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 }
